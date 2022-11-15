ff8f23984b32a15703b1d7bbb0e3594d2213a86f
boyuan <dengboyuan2001@gmail.com>
2022-11-14 22:35:57 -0800

    feat/ add connect signer button when signer not connected

--- src/components/Navbar/ZkAccountButton.tsx
+++ src/components/Navbar/ZkAccountButton.tsx
@@ -1,25 +1,50 @@
 import React, { useState } from 'react';
-import OutsideClickHandler from 'react-outside-click-handler';
-import ZkAccountModal from '../ZkAccoutModal';
 import MantaIcon from 'resources/images/manta.png';
+import OutsideClickHandler from 'react-outside-click-handler';
+import { usePrivateWallet } from 'contexts/privateWalletContext';
+import { useModal } from 'hooks';
+import ConnectSignerModal from 'components/Modal/connectSigner';
+import ZkAccountModal from '../ZkAccoutModal';
 
 const ZkAccountButton = () => {
   const [showZkModal, setShowZkModal] = useState(false);
+  const { privateAddress } = usePrivateWallet();
+  const { ModalWrapper, showModal } = useModal();
 
-  return (
-    <div className="relative">
-      <OutsideClickHandler onOutsideClick={() => setShowZkModal(false)}>
-        <div
-          className="flex gap-3 py-3 px-4 bg-secondary text-secondary font-medium cursor-pointer rounded-lg"
-          onClick={() => setShowZkModal(!showZkModal)}
+  const ZkAccountDisplay = () => {
+    return (
+      <div className="relative">
+        <OutsideClickHandler onOutsideClick={() => setShowZkModal(false)}>
+          <div
+            className="flex gap-3 py-3 px-4 bg-secondary text-secondary font-medium cursor-pointer rounded-lg"
+            onClick={() => setShowZkModal(!showZkModal)}
+          >
+            <img className="w-6 h-6" src={MantaIcon} alt="Manta" />
+            zkAddress
+          </div>
+          {showZkModal && <ZkAccountModal />}
+        </OutsideClickHandler>
+      </div>
+    );
+  };
+
+  const ZkAccountConnect = () => {
+    return (
+      <>
+        <button
+          className="btn-secondary py-3 px-4 font-medium cursor-pointer rounded-lg"
+          onClick={showModal}
         >
-          <img className="w-6 h-6" src={MantaIcon} alt="Manta" />
-          zkAddress
-        </div>
-        {showZkModal && <ZkAccountModal />}
-      </OutsideClickHandler>
-    </div>
-  );
+          Connect Signer
+        </button>
+        <ModalWrapper>
+          <ConnectSignerModal />
+        </ModalWrapper>
+      </>
+    );
+  };
+
+  return privateAddress ? <ZkAccountDisplay /> : <ZkAccountConnect />;
 };
 
 export default ZkAccountButton;
